c4af41b4fdf72852b8a25401de0ceb25b5ef2d72
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2023-04-19 11:49:43 -0400

    first step towards memory efficiency

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use reqwest::Error;
 use scraper::{Html, Selector};
 use serde::{Deserialize, Serialize};
 use serde_json;
+use std::collections::{BTreeMap, HashMap};
 use url::{ParseError, Url};
 
 #[tokio::main]
@@ -11,11 +12,11 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-async fn get_courses() -> Result<Vec<Course>, Error> {
+async fn get_courses() -> Result<BTreeMap<String, String>, Error> {
+    let mut courses = BTreeMap::new();
     let url = "https://stevens.smartcatalogiq.com/Institutions/Stevens-Institution-of-Technology/json/2022-2023/Academic-Catalog.json";
     let response = reqwest::get(url).await?;
     let l1 = &response.json::<serde_json::Value>().await?["Children"][23];
-    let mut courses: Vec<Course> = vec![];
     for c1 in Counter::new(0) {
         let l2 = &l1["Children"][c1];
         if l2.is_null() {
@@ -31,12 +32,41 @@ async fn get_courses() -> Result<Vec<Course>, Error> {
                 if l4.is_null() {
                     break;
                 }
-                let course = serde_json::from_value(l4.clone()).unwrap();
-                courses.push(course);
+                let course: Course = serde_json::from_value(l4.clone()).unwrap();
+                courses.insert(
+                    course.name,
+                    course
+                        .link
+                        .split_once("/2022-2023/Academic-Catalog/Courses/")
+                        .unwrap()
+                        .1
+                        .to_string(),
+                );
             }
         }
     }
     Ok(courses)
+}
+
+fn get_link(course_id: String, course_mappings: HashMap<String, String>) -> Option<String> {
+    let shared =
+        String::from("https://stevens.smartcatalogiq.com/en/2022-2023/Academic-Catalog/Courses/");
+    let (prefix, id) = match course_id.split_once(' ') {
+        Some(tuple) => (tuple.0, tuple.1),
+        _ => return None,
+    };
+    let department = match course_mappings.get(prefix) {
+        Some(value) => value,
+        _ => return None,
+    };
+    let level = match id.len() {
+        3 => format!("{}00", id.chars().next().unwrap()),
+        1 | 2 => String::from('0'),
+        _ => return None,
+    };
+    Some(format!(
+        "{shared}{prefix}-{department}/{level}/{prefix}-{id}"
+    ))
 }
 
 #[derive(Serialize, Deserialize, Debug)]
